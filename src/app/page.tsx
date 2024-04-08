bb664f2b84475c017a316514c755bdc4e13014cc
Iannn <pristianb007@gmail.com>
2024-04-08 09:30:37 +0700

    Search Bar

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,24 @@
-import { Hero } from '@/components';
+import { CustomFilter, Hero, SearchBar } from '@/components';
 
 export default function Home() {
   return (
     <main className='overflow-hidden'>
       <Hero />
+
+      <div className='mt-12 padding-x padding-y max-width' id='discover'>
+        <div className='home__text-container'>
+          <h1 className='text-4xl font-extrabold'>Car Catalogue</h1>
+          <p>Explore all of our Cumi Darat Cars and find the perfect one for you.</p>
+        </div>
+
+        <div className='home__filters'>
+          <SearchBar />
+          <div className='home__filter-container'>
+            <CustomFilter title='fuel' />
+            <CustomFilter title='year' />
+          </div>
+        </div>
+      </div>
     </main>
   );
 }
--- src/components/index.ts
+++ src/components/index.ts
@@ -2,5 +2,16 @@ import CustomButton from './shared/button/CustomButton';
 import Hero from './Hero';
 import Navbar from './shared/Navbar';
 import Footer from './shared/Footer';
+import SearchBar from './shared/filter/SearchBar';
+import CustomFilter from './shared/filter/CustomFilter';
+import SearchManufacturer from './shared/filter/SearchManufacturer';
 
-export { Hero, CustomButton, Navbar, Footer };
+export {
+  Hero,
+  CustomButton,
+  Navbar,
+  Footer,
+  SearchBar,
+  CustomFilter,
+  SearchManufacturer,
+};
